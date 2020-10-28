8778e8e736e9708fa8c083cddf1fbf92ba1384b5
Rachel Peng <rachelvpeng@gmail.com>
2020-10-28 18:10:12 -0400

    express app containing the required elements for ps4

---
+++ routes/ps4.js
@@ -0,0 +1,41 @@
+let express = require('express');
+let request = require('request');
+let router = express.Router();
+
+const OPTIONS = require('../config/options')
+
+const getPokemonAbility = (pokemonName) => {
+  return new Promise(function (resolve, reject) {
+    const options = {
+      'method': 'GET',
+      'url': OPTIONS.getPokemonAbilityOptions.url + pokemonName,
+      'headers': {
+        'cookie': OPTIONS.getPokemonAbilityOptions.headers.Cookie
+      }
+    };
+
+    request(options, function (error, response) {
+      if (error) return reject(error);
+      try {
+        resolve(JSON.parse(response.body).abilities[0].ability.name);
+      } catch (e) {
+        reject(e)
+      }
+    });
+  });
+}
+
+router.route('/')
+  .get((req, res, next) => {
+    res.render('form');
+  })
+  .post((req, res, next) => {
+    let pokemonName = req.body.pokemon;
+    getPokemonAbility(pokemonName)
+      .then(response => {
+        res.render('renderAbility', { pokemonName: pokemonName, ability: response });
+      })
+      .catch(err => console.log(err));
+  })
+
+module.exports = router;
