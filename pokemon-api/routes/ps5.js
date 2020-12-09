20f3b328010dc5afef492ab847b82055d2702e5f
Rachel Peng <rachelvpeng@gmail.com>
2020-12-09 16:22:47 -0500

    sldkf

---
+++ pokemon-api/routes/ps5.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const request = require('request');
+const router = express.Router();
+
+const redis = require('redis');
+const client = redis.createClient();
+
+const OPTIONS = require('../config/options')
+
+client.flushdb((err, success) => {
+  if (err) {
+    throw new Error(err);
+  }
+})
+
+const getPokemonAbility = (pokemonName) => {
+  return new Promise(function (resolve, reject) {
+    const options = {
+      'method': 'GET',
+      'url': OPTIONS.getPokemonAbilityOptions.url + pokemonName,
+      'headers': {
+        'cookie': OPTIONS.getPokemonAbilityOptions.headers.Cookie
+      }
+    };
+
+    request(options, function (error, response) {
+      if (error) reject(error);
+
+      resolve(JSON.parse(response.body).abilities[0].ability.name);
+    });
+  });
+}
+
+router.route('/')
+  .get((req, res, next) => {
+    res.render('form');
+  })
+  .post((req, res, next) => {
+    let pokemonName = req.body.pokemon;
+
+    client.exists(pokemonName, (err, match) => {
+      if (err) {
+        throw new Error(err);
+      }
+
+      if (match) {
+        client.get(pokemonName, (e, pokemonAbility) => {
+          if (e) throw new Error(e);
+
+          let response = {
+            pokemonAbility: pokemonAbility,
+            fromCache: true
+          }
+
+          res.send(response);
+        })
+      } else {
+        getPokemonAbility(pokemonName)
+          .then(pokemonAbility => {
+            client.set(pokemonName, pokemonAbility, (e, r) => {
+              if (e) throw new Error(e);
+              
+              let response = {
+                pokemonAbility: pokemonAbility,
+                fromCache: false
+              }
+
+              res.send(response);
+            })
+
+            client.expire(pokemonName, 15);
+          })
+      }
+    })
+  })
+
+module.exports = router;
